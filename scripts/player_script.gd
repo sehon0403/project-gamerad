1deaa5affd957ad0b72c2510170ec736f44dd001
noofne <sehonlee0403@gmail.com>
2025-08-06 19:16:56 +0200

    fixed script attachment

--- scripts/player_script.gd
+++ scripts/player_script.gd
@@ -1,8 +1,8 @@
-extends StaticBody2D
+extends Node2D
 const arr_ext = preload("res://scripts/array_ext.gd")
 const dict_ext = preload("res://scripts/dictionary_ext.gd")
 
-@export var PLAYER_SPEED = 5
+@export var PLAYER_SPEED = 500
 
 var DIR_VECTORS = {
 	"move_up"   : Vector2(0.0, -1.0),
@@ -15,15 +15,16 @@ func _ready() -> void:
 	print("Player Node ready!")
 	print("NOTE: arctan2(0, 0) resolves to " + str(atan2(0, 0)) + " in GDScript")
 	
-func handle_movement() -> void:
+func handle_movement(delta: float) -> void:
 	var mvmt_arrays: Array[Vector2] = []
+	
 	for dir_packet in dict_ext.items(DIR_VECTORS):
 		if Input.is_action_pressed(dir_packet[0]):
 			mvmt_arrays.append(dir_packet[1])
 
 	var mvmt_vector = arr_ext.v2_sum(mvmt_arrays)
-	
 	var velocity
+	
 	if mvmt_vector.x == 0 and mvmt_vector.y == 0:
 		# still condition
 		velocity = Vector2(0.0, 0.0)
@@ -31,7 +32,7 @@ func handle_movement() -> void:
 		var theta = atan2(mvmt_vector.y, mvmt_vector.x) # in radians
 		velocity = Vector2(PLAYER_SPEED*cos(theta), PLAYER_SPEED*sin(theta))
 	
-	self.position += velocity
+	self.global_position += velocity * delta
 
-func _physics_process(_delta: float) -> void:
-	handle_movement()
+func _physics_process(delta: float) -> void:
+	handle_movement(delta)
